cc1d2309f60346d93daea9b7e6c092519f5b1d97
AnthoineSG <anthoine93@outlook.fr>
2022-03-30 19:24:28 +0200

    page dacc ok + style ok

---
+++ serve.js
@@ -0,0 +1,22 @@
+const express = require("express"); // utilise express
+const app = express();
+const PORT = 8080; // le port utiliser de base
+
+app.set("view engine", "ejs"); // utilise ejs
+app.set("views", __dirname + "/app/views"); // utilise ejs dans les fichier du doss "views"
+
+app.use(express.static("statics")); // css/js dans doss "statics"
+
+
+const router = require("./app/routes/rHomePage"); // va chercher les fichier a partire de routeur
+
+
+
+app.use(router);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Le serve est lancer et ecoute a : http://localhost:${PORT}`);
+});
